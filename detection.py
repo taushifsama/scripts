1a8cdc9bf618eb4b71d120046332c8367f4eaced
taushifsama <67318657+taushifsama@users.noreply.github.com>
2020-11-20 21:35:54 +0530

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,107 @@
+import os
+import cv2
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
+import pathlib
+import tensorflow as tf
+
+tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
+
+# Enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+import time
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+
+PATH_TO_TEST_IMAGES_DIR = "C:\\Users\\Tausif\\Downloads\\mask_dataset"
+image_paths=[]
+filenames = ['image0947.jpg','image0953.jpg','image0931.jpg','image0877.jpg','image0848.jpg','image0811.jpg','image0799.jpg','image1065.jpg','image1011.jpg','image1093.jpg']
+for filename in filenames:
+    IMAGE_PATH = os.path.join(PATH_TO_TEST_IMAGES_DIR,filename)
+    image_paths.append(str(IMAGE_PATH))
+
+
+PATH_TO_SAVED_MODEL = str(r"C:\Users\Tausif\Documents\tensorflow\training\exported-models\my_model/saved_model")
+PATH_TO_LABELS = str(r"C:\Users\Tausif\Documents\tensorflow\training\annotations\label_map.pbtxt")
+
+print('Loading model...', end='')
+start_time = time.time()
+
+# Load saved model and build the detection function
+detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Done! Took {} seconds'.format(elapsed_time))
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
+                                                                    use_display_name=True)
+
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
+i=0
+
+def load_image_into_numpy_array(path):
+    return np.array(Image.open(path))
+
+
+for image_path in image_paths:
+
+    print('Running inference for {}... '.format(image_path), end='')
+
+    image_np = load_image_into_numpy_array(image_path)
+
+    # Things to try:
+    # Flip horizontally
+    # image_np = np.fliplr(image_np).copy()
+
+    # Convert image to grayscale
+    # image_np = np.tile(
+    #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image_np)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    #input_tensor = np.expand_dims(image_np, 0)
+    detections = detect_fn(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                   for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+          image_np_with_detections,
+          detections['detection_boxes'],
+          detections['detection_classes'],
+          detections['detection_scores'],
+          category_index,
+          use_normalized_coordinates=True,
+          max_boxes_to_draw=200,
+          min_score_thresh=.30,
+          agnostic_mode=False)
+
+    plt.figure()
+    plt.imshow(image_np_with_detections)
+    plt.xticks([]), plt.yticks([])
+    plt.savefig('C:/Users/Tausif/Desktop/output_image{}.jpg'.format(i))
+    i=i+1
+    print('Done')
+plt.show()
+
+# sphinx_gallery_thumbnail_number = 2
